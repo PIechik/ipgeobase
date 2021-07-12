4adbc1ebbeb1ba79d9a98e6e29c80012e8af5e47
Polina Miftakhudinova <rambeeech@gmail.com>
2021-07-12 15:32:34 +0400

    Tests and logic of gem

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 
 source "https://rubygems.org"
 
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}"}
+
 # Specify your gem's dependencies in ipgeobase.gemspec
 gemspec
 
@@ -10,3 +12,13 @@ gem "rake", "~> 13.0"
 gem "minitest", "~> 5.0"
 
 gem "rubocop", "~> 1.7"
+
+gem 'nokogiri-happymapper', require: 'happymapper'
+
+gem 'addressable'
+
+gem 'net-http'
+
+group :test do
+  gem "webmock"
+end
--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require "net/http"
+require "addressable/uri"
+require "happymapper"
 
 module Ipgeobase
   class Error < StandardError; end
   # Your code goes here...
+  def self.lookup(ip)
+    uri = Addressable::URI.parse("http://ip-api.com/xml/#{ip}?fields=24787")
+    res = Net::HTTP.get_response(uri)
+    HappyMapper.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+  end
 end
